a81b658bac9346166ca7e97fedb1a1680681633d
Hankey84 <hankeybanister84@gmail.com>
2023-12-10 19:58:01 +0300

    Add more powerfull version of webServer

--- src/main/java/ru/geekbrains/WebServer.java
+++ src/main/java/ru/geekbrains/WebServer.java
@@ -1,4 +1,4 @@
-package ru.geekbrains;
+package ru.geekbrain;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -7,33 +7,61 @@ import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class WebServer {
+
+    private static String WWW = "/Users/macbook/IdeaProjects/first-geek-web-server/www";
+
     public static void main(String[] args) {
+        try (ServerSocket serverSocket = new ServerSocket(8080)) {
+            System.out.println("Server started!");
 
-        try (ServerSocket serverSocket = new ServerSocket(8088)) {
-         while(true) {
-             Socket socket = serverSocket.accept();
-             System.out.println("New client connected!");
+            while (true) {
+                Socket socket = serverSocket.accept();
+                System.out.println("New client connected!");
 
-             BufferedReader input = new BufferedReader(
-                     new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8));
-             PrintWriter output = new PrintWriter(socket.getOutputStream());
+                new Thread(() -> handleRequest(socket)).start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-             while (!input.ready()) ;
+    private static void handleRequest(Socket socket) {
+        try (BufferedReader input = new BufferedReader(
+                new InputStreamReader(
+                        socket.getInputStream(), StandardCharsets.UTF_8));
+             PrintWriter output = new PrintWriter(socket.getOutputStream())
+        ) {
+            while (!input.ready());
 
-             while (input.ready()) {
-                 System.out.println(input.readLine());
-             }
-             output.println("HTTP/1.1 200 OK");
-             output.println("Content-Type: text/html; charset=utf-8");
-             output.println();
-             output.println("<h1>Привет от сервера!</h1>");
-             output.flush();
+            String firstLine = input.readLine();
+            String[] parts = firstLine.split(" ");
+            System.out.println(firstLine);
+            while (input.ready()) {
+                System.out.println(input.readLine());
+            }
 
-             input.close();
-             output.close();
-         }
+            Path path = Paths.get(WWW, parts[1]);
+            if (!Files.exists(path)) {
+                output.println("HTTP/1.1 404 NOT_FOUND");
+                output.println("Content-Type: text/html; charset=utf-8");
+                output.println();
+                output.println("<h1>Файл не найден!</h1>");
+                output.flush();
+                return;
+            }
+
+            output.println("HTTP/1.1 200 OK");
+            output.println("Content-Type: text/html; charset=utf-8");
+            output.println();
+
+            Files.newBufferedReader(path).transferTo(output);
+
+            System.out.println("Client disconnected!");
         } catch (IOException e) {
             e.printStackTrace();
         }
